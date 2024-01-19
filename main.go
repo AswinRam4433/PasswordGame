41008c11007f805c2f5fc73678117ec1c60ea6f6
Aswin Ramanathan <aswinarv55@gmail.com>
2024-01-19 22:33:06 +0530

    Rudimentary Roman Numerals

--- main.go
+++ main.go
@@ -50,7 +50,6 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -172,106 +171,70 @@ func rule8(s string) bool {
 
 }
 
-func countRomanNumerals(s string) map[string]int {
-	rome := []string{"I", "V", "X", "L", "C", "D", "M"}
-	counts := make(map[string]int)
+// func rule9(s string) bool {
+// 	// Roman Numerals Multiply to 35
+// 	pat1 := `^[A-Za-z0-9]*V[A-Za-z0-9]*VII[A-Za-z0-9]$`
+// 	matched, err := regexp.Match(pat1, []byte(s))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println((matched))
+// 	return true
 
-	for _, numeral := range rome {
-		counts[numeral] = strings.Count(s, numeral)
-	}
-
-	return counts
-}
+// }
 
 func rule9(s string) bool {
-	// Roman numerals must multiply to 35
-	values := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-	counts := countRomanNumerals(s)
-
-	product := 1
-	for numeral, count := range counts {
-		product *= values[numeral] ^ count
+	// Roman Numerals Multiply to 35
+	pat1 := `^[A-Za-z0-9]*V[A-Za-z0-9]*VII[A-Za-z0-9]*$`
+	matched, err := regexp.Match(pat1, []byte(s))
+	if err != nil {
+		panic(err)
+	}
+	if matched == true {
+		return true
 	}
 
-	return product == 35
-}
-
-var romanNumerals = map[string]int{
-	"I":  1,
-	"IV": 4,
-	"V":  5,
-	"IX": 9,
-	"X":  10,
-	"XL": 40,
-	"L":  50,
-	"XC": 90,
-	"C":  100,
-	"CD": 400,
-	"D":  500,
-	"CM": 900,
-	"M":  1000,
-}
-
-func romanToDecimal(s string) (int, error) {
-	total := 0
-	for len(s) > 0 {
-		// Sort numerals in descending order to match longer numerals first
-		var numerals []string
-		for numeral := range romanNumerals {
-			numerals = append(numerals, numeral)
-		}
-		sort.Slice(numerals, func(i, j int) bool {
-			return len(numerals[i]) > len(numerals[j])
-		})
-
-		// Check for longer numerals first
-		for _, numeral := range numerals {
-			if len(s) >= len(numeral) && s[:len(numeral)] == numeral {
-				total += romanNumerals[numeral]
-				s = s[len(numeral):]
-				break
-			}
-		}
+	pat2 := `^[A-Za-z0-9]*VII[A-Za-z0-9]*V[A-Za-z0-9]*$`
+	matched1, err1 := regexp.Match(pat2, []byte(s))
+	if err1 != nil {
+		panic(err1)
 	}
-	if len(s) > 0 {
-		return 0, fmt.Errorf("Invalid Roman numeral: %s", s)
-	}
-	return total, nil
-}
-
-func findRomanNumerals(s string) map[string]int {
-	result := make(map[string]int)
-
-	// Define a regular expression to match Roman numerals
-	regex := regexp.MustCompile(`(?:` + getRomanNumeralPattern() + `)+`)
-
-	// Find all matches in the input string
-	matches := regex.FindAllString(s, -1)
-
-	// Convert Roman numerals to decimal values and store in the result map
-	for _, match := range matches {
-		value, err := romanToDecimal(match)
-		if err == nil {
-			result[match] = value
-		}
+	if matched1 == true {
+		return true
 	}
 
-	return result
-}
-
-func getRomanNumeralPattern() string {
-	pattern := ""
-	for numeral := range romanNumerals {
-		// Ensure shorter numerals are matched before longer ones
-		pattern = numeral + "|" + pattern
+	pat3 := `^[A-Za-z0-9]*XXXV[A-Za-z0-9]*$`
+	matched2, err2 := regexp.Match(pat3, []byte(s))
+	if err2 != nil {
+		panic(err1)
 	}
-	// Trim the trailing "|"
-	return pattern[:len(pattern)-1]
-}
+	if matched2 == true {
+		return true
+	}
 
+	pat4 := `^[A-Za-z0-9]*XXXV[A-Za-z0-9]*I[A-Za-z0-9]*$`
+	matched3, err3 := regexp.Match(pat4, []byte(s))
+	if err3 != nil {
+		panic(err1)
+	}
+	if matched3 == true {
+		return true
+	}
+
+	pat5 := `^[A-Za-z0-9]*I[A-Za-z0-9]*XXXV[A-Za-z0-9]*$`
+	matched4, err4 := regexp.Match(pat5, []byte(s))
+	if err4 != nil {
+		panic(err1)
+	}
+	if matched4 == true {
+		return true
+	}
+
+	return false
+}
 func main() {
 	fmt.Println("The Go code is running")
-	s := "abc12A%778marchpepsiVVII"
+	s := "abc12A778marchpepsiVgkgkjVII"
 	// s1 := "abcdefg"
 	// s2 := "abcd1234"
 	// s3 := "ABCDEFGH"
@@ -285,15 +248,8 @@ func main() {
 	// fmt.Println(rule6(s))
 	// fmt.Println(rule7(s))
 	// fmt.Println(rule8(s))
-	// fmt.Println(rule9(s))
+	fmt.Println(rule9(s))
 	// fmt.Println(rule10(s))
-
-	result := findRomanNumerals(s)
-
-	// Print the results
-	for numeral, value := range result {
-		fmt.Printf("Roman numerals found: %s = %d\n", numeral, value)
-	}
 
 	// fmt.Println(rule5(s))
 
